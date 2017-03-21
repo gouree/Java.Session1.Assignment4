d3810738be71e15f66e9f76092730258a1eedef3
gouree <goure.deshpande@gmail.com>
2017-03-21 20:11:40 +0530

    Add files via upload

---
+++ Assignment4/src/CharactersAsciiValues.java
@@ -0,0 +1,15 @@
+
+public class CharactersAsciiValues {
+
+		public static void main (String args[])
+		{
+			
+			
+			int i=65;
+			   for(char alpha=65; alpha<=90 ;alpha++)
+			   {
+			       System.out.println(i+" -->"+alpha);
+			       i++;
+			   }
+		}
+}
